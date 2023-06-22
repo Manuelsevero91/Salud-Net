bbe927e1fcfc4bca53f7cabe77851129096f5220
Manuel <manusevero91@gmail.com>
2023-06-22 10:21:08 -0300

    Agregue formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Body from './Componentes/Body'
 
 function App() {
   
 
   return (
     <>
-    
+     <Body/>
     </>
   )
 }
 
-export default App
+export default App;
