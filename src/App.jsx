597da0b39d953f3cdabe4050ad511f05b98e4850
karinadesposito <110677129+karinadesposito@users.noreply.github.com>
2023-06-22 00:35:51 -0300

    Agregue, Body y NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
+import './styles/NavBar.css'
+import './styles/Body.css'
+
+import Body from './views/components/Body'
+import NavBar from './views/components/NavBar'
 
 function App() {
-  
+ 
 
   return (
-    <>
-    
+    <>    
+    <NavBar/>
+    <Body/>
+      {
+      }
     </>
   )
 }
