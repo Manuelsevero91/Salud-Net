f967895f39b60da75dec5cafb4c2fafc1e6e5b13
karinadesposito <110677129+karinadesposito@users.noreply.github.com>
2023-06-24 12:25:35 -0300

    body arreglado

--- src/Componentes/Body.jsx
+++ src/Componentes/Body.jsx
@@ -2,35 +2,8 @@ import React from 'react';
 // import Users from '.Users'
 import {useEffect, useState} from 'react'
 
-
-const Formulario = () => {
-    //Estados del formulario//
-  const [nombre, setNombre] = useState('');
-  const [email, setEmail] = useState('');
-  const [consulta, setConsulta] = useState('');
-
-  //Manejo de eventos//
-  const handleNombreChange = (event) => {
-    setNombre(event.target.value);
-  };
-
- const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleConsultaChange = (event) => {
-    setConsulta(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Mostrar datos en consola//
-    console.log('Nombre:', nombre);
-    console.log('Email:', email);
-    console.log('Consulta:', consulta);
-
-  };
-
+function Body() {
+  
   return (
     <>
     <div className = "container">
