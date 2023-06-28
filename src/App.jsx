27b14a9f29aa5393a8b9f8cc6691a82df5fd032a
Manuel <manusevero91@gmail.com>
2023-06-28 10:39:08 -0300

    Agregue componente contacto, rutas ingresar y contacto funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,27 @@ import "./Styles/NavBar.css";
 import "./Styles/Body.css";
 import "./Styles/Form.css";
 import "./Styles/Footer.css";
-import Body from "./Componentes/Body";
+import Home from "./Componentes/Home";
 import NavBar from "./Componentes/NavBar";
-import Footer from "./Componentes/Footer";
 import { Routes, Route } from 'react-router-dom';
-import {useState} from 'react'
-import Form from "./Componentes/Form";
 import Login from "./Componentes/Login";
+import Contacto from "./Componentes/Contacto";
 
 function App() {
-  const [showContactForm, setShowContactForm] = useState(false);
-
-  const handleContactClick = () => {
-    setShowContactForm(true);
-  };
-
+ 
   return (
     <>
     <NavBar/>
     <Routes>
-      {/* <Route exact path = "/" element = {<Header/>} showContactForm={showContactForm} handleContactClick={handleContactClick}/> */}
-      <Route exact path="/" element = {<Body/>} />
-      {/* <Route exact path="/" element = {<Footer/>} /> */}
-      <Route exact path="/" element = {<Form/>} />
-      <Route exact path="/" element = {<Login/>} />
+      <Route exact path="/" element = {<Home/>} />
+      <Route exact path="/contacto" element = {<Contacto/>} />
+      <Route exact path="/ingresar" element = {<Login/>} />
       </Routes>
-      <Footer/>
-      
+    
+
     </>
   );
 }
 
 export default App;
+
---
+++ src/Componentes/Contacto.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Form from "./Form";
+
+function Contacto() {
+  const location = useLocation();
+  const [showContactForm, setShowContactForm] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname === "/contacto") {
+      setShowContactForm(true);
+    }
+  }, [location]);
+
+  const handleSubmit = (formData) => {
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h2>Contacto</h2>
+      {showContactForm && <Form onSubmit={handleSubmit} />}
+    </div>
+  );
+}
+
+
+export default Contacto;
