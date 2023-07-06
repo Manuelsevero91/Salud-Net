08f27c230c4a10347f638e55ad5ceab5b80aa087
Manuel <manusevero91@gmail.com>
2023-07-06 01:38:41 -0300

    Iconos footer redireccionando

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,22 +1,17 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebook,
-  faTwitter,
-  faInstagram
-} from "@fortawesome/free-brands-svg-icons";
+import {faFacebook, faTwitter,faInstagram} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="social-icons">
-        <a href="#" className="social-icon">
+        <a href="https://www.facebook.com/" className="social-icon">
           <FontAwesomeIcon icon={faFacebook} />
         </a>
-        <a href="#" className="social-icon">
+        <a href="https://twitter.com/" className="social-icon">
           <FontAwesomeIcon icon={faTwitter} />
         </a>
-        <a href="#" className="social-icon">
+        <a href="https://www.instagram.com/" className="social-icon">
           <FontAwesomeIcon icon={faInstagram} />
         </a>
       </div>
