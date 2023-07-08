7693ae1bc8966424bbab3be10cde81afb442bcd7
karinadesposito <110677129+karinadesposito@users.noreply.github.com>
2023-07-08 00:13:44 -0300

    =agregado de foto, párrafo, arreglo de mensaje de formulario

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,33 @@
 import Marketing from '../Componentes/Marketing'
 import Footer from '../Componentes/Footer'
 
-function Home({isLoggedIn}) {
+function Home({ isLoggedIn }) {
 
   return (
     <>
-     <body class= 'inicio'>
-    <div className = "container">
-        {!isLoggedIn && <h1>Bienvenidos a Salud Net</h1>}
-    </div>
-    
-    <div className="nosotros">
-    <h2>Sobre Nosotros</h2>
-    <p>
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum."
-    </p>   
-  </div>
-  <Marketing />
-  <Footer/>
-  </body>
+      <body class='inicio'>
+        <div className="container">
+          {!isLoggedIn && <h1>Bienvenidos a Salud Net</h1>}
+        </div>
+
+        <div className="nosotros">
+          <h2>Sobre Nosotros</h2>
+          <p className="spaced-paragraph">
+            "SaludNet nació en el 2016 como iniciativa de un conjunto de profesionales
+            con la misión de mejorar la salud en las personas.
+            Nuestro propósito es ofrecer una calidad de atención eficaz  al
+            servicios de atención de la salud basados en la evidencia;
+            segura  al evitar daños a las personas a las que está destinado el cuidado;
+            oportuna  al reducir los tiempos de espera y eficiente  maximizando el beneficio
+            de los recursos disponibles y evitando el desperdicio.
+            Ofrecemos una gran variedad de especialidades para brindar un cuidado integral
+            que se base en la prevención de enfermedades y en la promoción de hábitos saludables
+            para una población con una creciente expectativa de vida."
+          </p>
+        </div>
+        <Marketing />
+        <Footer />
+      </body>
     </>
   );
 };
