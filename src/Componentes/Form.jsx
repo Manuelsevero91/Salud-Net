ec9a3863debded8230df163eea4e7539726a692d
Manuel <manusevero91@gmail.com>
2023-07-14 01:07:19 -0300

    Cambio form a componente

--- src/Componentes/Form.jsx
+++ src/Componentes/Form.jsx
@@ -6,7 +6,7 @@ const Formulario = () => {
   const [consulta, setConsulta] = useState("");
   const [enviado, setEnviado] = useState(false);
   const [cerrado, setCerrado] = useState(false);
-
+  const [errores, setErrores] = useState({});
 
   const handleNombreChange = (event) => {
     setNombre(event.target.value);
@@ -22,10 +22,41 @@ const Formulario = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Nombre:", nombre);
-    console.log("Email:", email);
-    console.log("Consulta:", consulta);
-    setEnviado(true);
+    const errores = validarFormulario();
+    if (Object.keys(errores).length === 0) {
+      console.log("Nombre:", nombre);
+      console.log("Email:", email);
+      console.log("Consulta:", consulta);
+      setEnviado(true);
+    } else {
+      setErrores(errores);
+    }
+  }
+  const validarFormulario = () => {
+    const errores = {};
+
+    if (nombre.trim() === "") {
+      errores.nombre = "Este campo es obligatorio";
+    }
+
+    if (email.trim() === "") {
+      errores.email = "Este campo es obligatorio";
+    } else {
+      const nombreApellido = nombre.trim().split(" ");
+      if (nombreApellido.length < 2) {
+        errores.nombre = "Debe ingresar el nombre y apellido";
+      }
+    }
+
+    if (consulta.trim() === "") {
+      errores.consulta = "Este campo es obligatorio";
+    }
+
+    if (nombre.trim() !== "" && nombre.indexOf(" ") === -1) {
+      errores.nombre = "Debe ingresar el nombre y apellido";
+    }
+
+    return errores;
   };
   const handleClose = () => {
     setCerrado(true)
@@ -59,30 +90,27 @@ const Formulario = () => {
           <button onClick={handleClose} className="close-button">Cerrar</button>
           <div>
             <label htmlFor="nombre">Nombre y Apellido</label>
-            <input
-              type="text"
+            <input type="text"
               id="nombre"
               value={nombre}
-              onChange={handleNombreChange}
-            />
+              onChange={handleNombreChange} />
+            {errores.nombre && <span className="error-label">{errores.nombre}</span>}
           </div>
           <div>
             <label htmlFor="email">Email:</label>
-            <input
-              type="email"
+            <input type="email"
               id="email"
               value={email}
-              onChange={handleEmailChange}
-            />
+              onChange={handleEmailChange} />
+            {errores.email && <span className="error-label">{errores.email}</span>}
           </div>
           <div>
             <label htmlFor="consulta">Espacio consulta:</label>
-            <input
-              type="text"
+            <input type="text"
               id="consulta"
               value={consulta}
-              onChange={handleConsultaChange}
-            />
+              onChange={handleConsultaChange} />
+            {errores.consulta && <span className="error-label">{errores.consulta}</span>}
           </div>
           <button id="btnSubContact" type="submit">Enviar</button>
         </form>
